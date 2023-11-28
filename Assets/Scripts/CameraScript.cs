db4e280922bcdcf1729798fa2049eeacb077ed8c
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2023-11-28 17:34:17 -0500

    fhfh

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,7 +13,7 @@ public class CameraScript : MonoBehaviour
 
     public GameManager manager;
 
-    public Animator camAnim;
+    
     // Start is called before the first frame update
     void Start()
     {
